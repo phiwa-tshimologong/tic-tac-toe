2c79201798fbb8280f46e0290d8058585d315667
phiwa-tshimologong <phiwa@dsa.tshimologong.joburg>
2022-02-22 13:43:33 +0200

    :fire: Added grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import Score from './Score'
+import style from './css/app.module.css'
 
 function App() {
   return (
     <div className="App">
       <Score/>
       
+      <div className={style.grid}>
+        <div className={style.cell}></div>
+        <div className={style.cell}></div>
+        <div className={style.cell}></div>
+        <div className={style.cell}></div>
+        <div className={style.cell}></div>
+        <div className={style.cell}></div>
+        <div className={style.cell}></div>
+        <div className={style.cell}></div>
+        <div className={style.cell}></div>
+      </div>
     </div>
   );
 }
--- src/Score.jsx
+++ src/Score.jsx
@@ -1,11 +1,13 @@
+import style from './css/score.module.css'
+
 const Score = () => {
     return(
-        <div className="score">
-            <div className="player1">
+        <div className={style.score}>
+            <div className="player-score">
                 <h1>Player 1: </h1>
             </div>
             <h1>Tic Tac Toe</h1>
-            <div className="player1">
+            <div className="player-score">
                 <h1>Player 2: </h1>
             </div>
         </div>
