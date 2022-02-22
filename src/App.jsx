6fda416c6e5339dcc5631f181c26dd61a021fcf8
phiwa-tshimologong <phiwa@dsa.tshimologong.joburg>
2022-02-22 11:47:26 +0200

    Add score component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Score from './Score'
+
+function App() {
+  return (
+    <div className="App">
+      <Score/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Score.jsx
@@ -0,0 +1,15 @@
+const Score = () => {
+    return(
+        <div className="score">
+            <div className="player1">
+                <h1>Player 1: </h1>
+            </div>
+            <h1>Tic Tac Toe</h1>
+            <div className="player1">
+                <h1>Player 2: </h1>
+            </div>
+        </div>
+    );
+}
+
+export default Score;
